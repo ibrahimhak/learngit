8ebb5fe1bda521ec369d1919e20cafb165cb273c
unknown <hakim@ebrahiem_lap.Cairo.TelecomEgypt.corp>
2023-07-22 12:04:37 +0300

    add calc code

---
+++ main.py
@@ -0,0 +1,47 @@
+while True:
+    while True:
+        try:
+          first_num = float(input("enter first number : "))
+          break
+        except ValueError:
+            print("invalid input. please enter a numeric value")
+    # 2- input operation 
+    while True:
+        try:
+          operation = input("enter operation type : ")
+          if operation in ("+", "-", "*", "/"):
+            break
+          else:
+            raise ValueError
+        except ValueError:
+            print("invalid operation, please enter + , - , * , / ")
+
+    # 3- input sec0nd number
+    while True:
+        try:
+          secound_number = float(input("enter secound number : "))
+          if secound_number == 0 and operation == "/":
+            raise ZeroDivisionError
+          break
+        except ValueError:
+            print("invalid input. please enter a numeric value")
+        except ZeroDivisionError:
+            print("cannot divide by zero, please enter another numeric")
+    if operation == "+":
+        print(first_num + secound_number)
+    elif operation == "-":
+        print(first_num + secound_number)     
+    elif operation == "*":
+        print(first_num * secound_number)   
+    elif operation == "/":
+        print(first_num / secound_number)  
+    else:
+        print("error")
+    repeat = input("do you want to perform another operation (y/n) : ")
+    if repeat == "n":
+        break
+    elif repeat == 'y':
+        continue
+    else:
+        print("please enter y to counten or n to cancel")
+print("program exited")
